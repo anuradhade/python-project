88b97a26acefdff53665a24ef9cdd54f29404b63
anuradhade <milan92.anude@gmail.com>
2023-01-08 00:24:23 +0530

    gitclasses

---
+++ user-defined.py
@@ -0,0 +1,12 @@
+# Program to illustrate
+# the use of user-defined functions
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
