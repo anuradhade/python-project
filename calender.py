cdf95ad57b513c1844ecefe573afb42fa942a7dc
anuradhade <milan92.anude@gmail.com>
2023-01-16 20:46:30 +0530

    added

---
+++ calender.py
@@ -0,0 +1,17 @@
+#program to display calender of the given month and Year
+
+#import calender module
+import calendar
+
+
+#yy = 2023 # year
+#mm = 11 # month
+
+#input the month and year from user
+yy = int(input("Enter year:"))
+mm = int(input("Enter Month:"))
+
+#Disply the calender
+
+#print(yy, mm)
+print(calendar.month(yy, mm))
